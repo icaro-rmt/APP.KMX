57c245ef1f6af9899482eb3a1a5b119b333bb891
Ícaro Rafael <icarorafa@gmail.com>
2024-11-03 15:55:53 +0000

    Commit de paginas atualizadas

---
+++ APP.KMX.App/Controllers/PlanosController.cs
@@ -0,0 +1,28 @@
+﻿using APP.KMX.App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APP.KMX.App.Controllers
+{
+    public class PlanosController : Controller
+    {
+        private readonly PlanModel _freeplan;
+        public PlanosController()
+        {
+            PlanModel freePlan = new PlanModel()
+            {
+                NomePlano = "Plano Gratuito",
+                QuantidadeConversoes= "1",
+                Formatos= "Formatos permitidos apenas tabelas .xls em mapas .kmz",
+                QuantidadeArquivos = "1 arquivo por vez",
+                QuantidadeUsuarios ="1",
+            };
+
+            _freeplan = freePlan;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_freeplan);
+        }
+    }
+}
---
+++ APP.KMX.App/Models/PlanModel.cs
@@ -0,0 +1,14 @@
+﻿namespace APP.KMX.App.Models
+{
+    public class PlanModel
+    {
+        public string NomePlano { get; set; } = string.Empty;
+        public string QuantidadeConversoes { get; set; } = string.Empty;
+        public string Formatos { get; set; } = string.Empty;
+        public string QuantidadeArquivos { get; set; } = string.Empty;
+        public string QuantidadeUsuarios { get; set; } = string.Empty;
+        public decimal? DePreco { get; set; }
+        public decimal? PorPreco { get; set; }
+        public decimal? Desconto { get; set; }
+    }
+}
